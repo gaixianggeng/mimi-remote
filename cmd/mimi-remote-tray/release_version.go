08593840a297c5b57c3a753ad0c198660d9f3e51
gaixg <gaixg94@gmail.com>
2026-08-23 23:32:58 +0800

    MIM-128 show release version in Windows panel (#247)

---
+++ cmd/mimi-remote-tray/release_version.go
@@ -0,0 +1,20 @@
+package main
+
+import "strings"
+
+// releaseVersion is injected by the Windows packaging script from the same
+// version value used for the installer and GitHub Release tag.
+var releaseVersion = "dev"
+
+func formatReleaseVersion(value string) string {
+	value = strings.TrimSpace(value)
+	if value == "" || strings.EqualFold(value, "dev") {
+		return "开发版"
+	}
+	value = strings.TrimPrefix(value, "v")
+	value = strings.TrimPrefix(value, "V")
+	if value == "" {
+		return "开发版"
+	}
+	return "v" + value
+}
